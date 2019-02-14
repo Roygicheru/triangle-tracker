ee21059e6a1979c470ccf6e7925ea3f581400a27
Roygicheru <gicheru.k.roy@gmail.com>
2019-02-14 16:08:28 +0300

    add condition to equilateral

--- triangle.js
+++ triangle.js
@@ -3,7 +3,19 @@ function findResult(){
   var b = prompt("side b")
   var c = prompt("side c")
 
-
+if(side a === side b && side a === side a && side b === side c){
+  alert("It is an equilateral triangle ")
+}
+ else if(){
+  alert("It is an isosceles triangle ")
+}
+else if(){
+  alert("It is not a triangle ")
+}
+else {
+  alert("It is an isosceles triangle")
+}
+}
 
 
 
