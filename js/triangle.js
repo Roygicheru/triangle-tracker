a6f4b63f50bff808ed60bb4a34705c53031c1912
Roygicheru <gicheru.k.roy@gmail.com>
2019-02-14 16:32:16 +0300

    Add all changes made

---
+++ js/triangle.js
@@ -0,0 +1,44 @@
+function findResult(){
+  var a = prompt("side a")
+  var b = prompt("side b")
+  var c = prompt("side c")
+
+if(side a === side b && side a === side a && side b === side c){
+  alert("It is an equilateral triangle ")
+}
+ else if(){
+  alert("It is an isosceles triangle ")
+}
+else if(){
+  alert("It is not a triangle ")
+}
+else {
+  alert("It is an scalene triangle")
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- triangle.js
+++ triangle.js
@@ -1,20 +1,19 @@
 function findResult(){
-  var a = prompt("side a")
-  var b = prompt("side b")
-  var c = prompt("side c")
+  var a = parseInt(prompt("side a"));
+  var b = prompt("side b");
+  var c = prompt("side c");
 
-if(side a === side b && side a === side a && side b === side c){
-  alert("It is an equilateral triangle ")
+if(side a === side b && side a === side c && side b === side c){
+  alert("It is an equilateral triangle ");
 }
  else if(){
-  alert("It is an isosceles triangle ")
+  alert("It is an isosceles triangle ");
 }
 else if(){
-  alert("It is not a triangle ")
+  alert("It is not a triangle ");
 }
 else {
-  alert("It is an scalene triangle")
-}
+  alert("It is an scalene triangle");
 }
 
 
