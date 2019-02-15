136b2d7cf6120585b97eb868423dd36b0168edee
Roygicheru <gicheru.k.roy@gmail.com>
2019-02-15 09:33:17 +0300

    add condition to scalene

--- triangle.js
+++ triangle.js
@@ -1,47 +1,21 @@
 function findResult() {
-  var a = prompt("sideA");
+  var a = prompt("Enter sideA");
   var b = prompt("sideB");
   var c = prompt("sideC");
 
-if( sideA === sideB && sideA === sideC && sideB === sideC);{
+if( sideA === sideB && sideA === sideC && sideB === sideC){
   alert("It is an equilateral triangle ");
-}
 }
 
  else if(sideA === sideB || sideA === sideC || sideB === sideC){
   alert("It is an isosceles triangle ");
 }
 
-else if(){
+else if((sideA + sideB) <= sideC || (sideA + sideC) <= sideB || (sideB + sideC) <= sideB){
   alert("It is not a triangle ");
 }
 
 else {
   alert("It is an scalene triangle");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
